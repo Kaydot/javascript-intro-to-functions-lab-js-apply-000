d90fbd5e86d810aaa9e88c8566afa523ab854019
k.rochelle.lewis@gmail.com <k.rochelle.lewis@gmail.com>
2018-08-08 15:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  var love = "I love you, Grandma";
+  var love = "I love you, Grandma.";
   var upper= string.toUpperCase();
   var lower= string.toLowerCase();
   
     if (string === love) 
-      return "I love you too.";
+      return "I love you, too.";
     
     if (string === upper)
       return "YES INDEED!";
